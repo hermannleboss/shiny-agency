2cd43bc53409ce76bb70720fb4261c0064dd1c83
EHO K. Hermann <hermannleboss@gmail.com>
2022-01-01 21:25:01 +0000

    refactor for initialisation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,6 @@
-import logo from '../../logo.svg';
-import '../../App.css'
-
-function Index() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/Index.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+function Home() {
+  return <div>Page d'accueil 🏡</div>
 }
 
-export default Index;
+export default Home
