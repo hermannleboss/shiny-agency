7f3b5836e5673506f3bf9ccf3c3c7dd8255e174e
EHO K. Hermann <hermannleboss@gmail.com>
2022-01-08 11:38:49 +0000

    refactor index

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom'
 import Home from './pages/Home'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Home />
+    </React.StrictMode>,
+    document.getElementById('root')
 )
+
+
+/**
+ * ReactDOM.render(
+ <React.StrictMode>
+ <Router>
+ <Route path="/">
+ <Home/>
+ </Route>
+ <Route path="/survey">
+ <Survey/>
+ </Route>
+ </Router>
+ </React.StrictMode>,
+ document.getElementById('root')
+ )
+
+ */
