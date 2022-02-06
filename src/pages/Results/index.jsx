1e7b79b3a73308ce8175f416a4912b0296bbfb41
EHO K. Hermann <hermannleboss@gmail.com>
2022-02-06 20:05:52 +0000

    rest result

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -55,7 +55,13 @@ const LoaderWrapper = styled.div`
   justify-content: center;
 `
 
-function formatFetchParams(answers) {
+export function formatJobList(title, listLength, index) {
+  if (index === listLength - 1) {
+    return title
+  }
+  return `${title},`
+}
+export function formatFetchParams(answers) {
   const answerNumbers = Object.keys(answers)
 
   return answerNumbers.reduce((previousParams, answerNumber, index) => {
@@ -94,8 +100,7 @@ function Results() {
             key={`result-title-${index}-${result.title}`}
             theme={theme}
           >
-            {result.title}
-            {index === resultsData.length - 1 ? '' : ','}
+            {formatJobList(result.title, resultsData.length, index)}
           </JobTitle>
         ))}
       </ResultsTitle>
