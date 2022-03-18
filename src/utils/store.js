3f66d3ededc7ca7934ef735e94ef307ec5ee62f4
EHO K. Hermann <hermannleboss@gmail.com>
2022-03-18 22:50:54 +0000

    survey reducer

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,13 @@
 import { combineReducers, createStore } from 'redux'
 import themeReducer from '../features/theme'
 import freelancesReducer from '../features/freelances'
+import surveyReducer from '../features/survey'
 
 const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const reducer = combineReducers({
   theme:      themeReducer,
-  freelances: freelancesReducer
+  freelances: freelancesReducer,
+  survey:     surveyReducer
 })
 
 const store = createStore(reducer, reduxDevtools)
