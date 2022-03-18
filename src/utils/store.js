cae833a1fce8d65e518fbbea5231bcca6224efa4
EHO K. Hermann <hermannleboss@gmail.com>
2022-03-18 17:00:16 +0000

    update store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,17 +1,11 @@
 import { combineReducers, createStore } from 'redux'
 import themeReducer from '../features/theme'
 
-// on utilise combineReducer pour faire
-// fonctionner plusieurs reducers ensemble
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const reducer = combineReducers({
-  // le themeReducer est responsable de la propriété `theme` du state
   theme: themeReducer
 })
 
-// on utilise le reducer créer avec combineReducers
-// pour initialiser le store
-// Pas besoin de passer de state initial
-// car chaque reducer à son propre state initial
-const store = createStore(reducer)
+const store = createStore(reducer, reduxDevtools)
 
 export default store
