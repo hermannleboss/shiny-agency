b49b98468528edaba1736102b01378dc7b50458a
EHO K. Hermann <hermannleboss@gmail.com>
2022-03-21 16:30:22 +0000

    refactor store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,17 +1,15 @@
-import { combineReducers, createStore } from 'redux'
 import themeReducer from '../features/theme'
 import freelancesReducer from '../features/freelances'
 import surveyReducer from '../features/survey'
 import freelanceReducer from '../features/freelance'
+import { configureStore } from '@reduxjs/toolkit'
 
-const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const reducer = combineReducers({
-  theme:      themeReducer,
-  freelances: freelancesReducer,
-  survey:     surveyReducer,
-  freelance:  freelanceReducer
+export default configureStore({
+  reducer: {
+    theme:      themeReducer,
+    freelances: freelancesReducer,
+    survey:     surveyReducer,
+    freelance:  freelanceReducer
+  }
 })
 
-const store = createStore(reducer, reduxDevtools)
-
-export default store
