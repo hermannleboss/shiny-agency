ffdb0fb5c39b12a040141d64499e6c66243a583c
EHO K. Hermann <hermannleboss@gmail.com>
2022-04-01 10:19:35 +0000

    Updates

---
+++ precache-manifest.353e2f38b88bea61a9b025dad93212bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "544507882303a32c8865",
+    "url": "/shiny-agency/build/static/js/main.2529b09f.chunk.js"
+  },
+  {
+    "revision": "be75f3dff84fe2f9b519",
+    "url": "/shiny-agency/build/static/js/runtime~main.6c01521a.js"
+  },
+  {
+    "revision": "980b34a5ca70f00a0db6",
+    "url": "/shiny-agency/build/static/js/2.ecab82f6.chunk.js"
+  },
+  {
+    "revision": "c1486bc7259bb83ecf0cc1d341d482e4",
+    "url": "/shiny-agency/build/static/media/home-illustration.c1486bc7.svg"
+  },
+  {
+    "revision": "352de2ad79c54f91d5ecc8ad02f754c1",
+    "url": "/shiny-agency/build/static/media/404.352de2ad.svg"
+  },
+  {
+    "revision": "3f9a231c355ba717c4263a683b1baaab",
+    "url": "/shiny-agency/build/static/media/empty.3f9a231c.svg"
+  },
+  {
+    "revision": "7794e5e639558cf4dde41d1158ec2a8a",
+    "url": "/shiny-agency/build/index.html"
+  }
+];
